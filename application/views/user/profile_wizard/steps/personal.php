3240383cd7dce56ab7396104ae0feb348bbb7bef
Iftikhar Ahmad Kashif <iftikhar@digitalwebsolution@gmail.com>
2026-05-19 16:23:56 +0500

    move cinc and dob to first row

--- application/views/user/profile_wizard/steps/personal.php
+++ application/views/user/profile_wizard/steps/personal.php
@@ -4,39 +4,6 @@
 
 <form id="profileFormStep1" class="signup-step-form" autocomplete="off">
 	<div class="form-row">
-		<div class="form-group col-md-4">
-			<label>Name<span class="text-danger">*</span></label>
-			<input type="text" class="form-control" name="name" placeholder="Enter full name" value="<?php echo html_escape($u->name ?? ''); ?>" required>
-		</div>
-		<div class="form-group col-md-4">
-			<label>Father Name<span class="text-danger">*</span></label>
-			<input type="text" class="form-control" name="father_name" placeholder="Enter father name" value="<?php echo html_escape($u->father_name ?? ''); ?>" required>
-		</div>
-		<div class="form-group col-md-4">
-			<label>Gender<span class="text-danger">*</span></label>
-			<?php $g = $u->gender ?? ''; ?>
-			<select class="form-control" name="gender" required>
-				<option value="">Select</option>
-				<option value="Male" <?php echo ($g === 'Male') ? 'selected' : ''; ?>>Male</option>
-				<option value="Female" <?php echo ($g === 'Female') ? 'selected' : ''; ?>>Female</option>
-				<option value="Other" <?php echo ($g === 'Other') ? 'selected' : ''; ?>>Other</option>
-			</select>
-		</div>
-	</div>
-
-	<div class="form-row">
-		<div class="form-group col-md-3">
-			<label>Phone Number<span class="text-danger">*</span></label>
-			<input type="text" class="form-control" name="phone" placeholder="03XXXXXXXXX" value="<?php echo html_escape($u->phone ?? ''); ?>" required>
-		</div>
-		<div class="form-group col-md-3">
-			<label>Date Of Birth<span class="text-danger">*</span></label>
-			<input type="date" class="form-control" name="dob" max="<?php echo html_escape($max_dob); ?>" value="<?php echo html_escape($u->dob ?? ''); ?>" required>
-		</div>
-		<div class="form-group col-md-3">
-			<label>Email Address<span class="text-danger">*</span></label>
-			<input type="email" class="form-control" name="email" placeholder="example@email.com" value="<?php echo html_escape($u->email ?? ''); ?>" required>
-		</div>
 		<div class="form-group col-md-3">
 			<label>CNIC<span class="text-danger">*</span></label>
 			<input
@@ -53,6 +20,39 @@
 				required
 			>
 			<div class="invalid-feedback" style="display:none">CNIC must be in format 12345-1234567-1</div>
+		</div>
+		<div class="form-group col-md-3">
+			<label>Date Of Birth<span class="text-danger">*</span></label>
+			<input type="date" class="form-control" name="dob" max="<?php echo html_escape($max_dob); ?>" value="<?php echo html_escape($u->dob ?? ''); ?>" required>
+		</div>
+		<div class="form-group col-md-3">
+			<label>Name<span class="text-danger">*</span></label>
+			<input type="text" class="form-control" name="name" placeholder="Enter full name" value="<?php echo html_escape($u->name ?? ''); ?>" required>
+		</div>
+		<div class="form-group col-md-3">
+			<label>Father Name<span class="text-danger">*</span></label>
+			<input type="text" class="form-control" name="father_name" placeholder="Enter father name" value="<?php echo html_escape($u->father_name ?? ''); ?>" required>
+		</div>
+	</div>
+
+	<div class="form-row">
+		<div class="form-group col-md-4">
+			<label>Gender<span class="text-danger">*</span></label>
+			<?php $g = $u->gender ?? ''; ?>
+			<select class="form-control" name="gender" required>
+				<option value="">Select</option>
+				<option value="Male" <?php echo ($g === 'Male') ? 'selected' : ''; ?>>Male</option>
+				<option value="Female" <?php echo ($g === 'Female') ? 'selected' : ''; ?>>Female</option>
+				<option value="Other" <?php echo ($g === 'Other') ? 'selected' : ''; ?>>Other</option>
+			</select>
+		</div>
+		<div class="form-group col-md-4">
+			<label>Phone Number<span class="text-danger">*</span></label>
+			<input type="text" class="form-control" name="phone" placeholder="03XXXXXXXXX" value="<?php echo html_escape($u->phone ?? ''); ?>" required>
+		</div>
+		<div class="form-group col-md-4">
+			<label>Email Address<span class="text-danger">*</span></label>
+			<input type="email" class="form-control" name="email" placeholder="example@email.com" value="<?php echo html_escape($u->email ?? ''); ?>" required>
 		</div>
 	</div>
 
